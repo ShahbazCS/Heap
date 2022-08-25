feaa48989227d86be2b65a9a7b6fc285711b8ad9
Shahbaz Khan <72274984+ShahbazCS@users.noreply.github.com>
2022-08-26 01:11:02 +0530

    Create 1636. Sort Array by Increasing Frequency.cpp

---
+++ 1636. Sort Array by Increasing Frequency.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        
+          unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        //map built
+        
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>minh;
+        for(auto i=mp.begin();i!=mp.end();i++){
+            minh.push({i->second,i->first});
+           // if(minh.size()>k) minh.pop();
+        }
+        //heap built
+        int n=nums.size();
+         vector<int>v(n);
+        int i,j=0;
+        int l=minh.size();
+        while(l--){
+            for(i=j;i<j+minh.top().first;i++){
+                v[i]=minh.top().second;
+            }
+            j=i;
+            minh.pop();
+        }
+        // vector built according to min freq, heap empty
+        
+         for(i=0;i<n-1; i=j){
+            j=i+1;
+            while(mp[v[i]]==mp[v[j]]){
+                j++;
+                if(j>=n) break;
+            }
+        
+              sort(v.begin()+i,v.begin()+j,greater<int>());
+           
+        }
+     return v;    
+    }
+};
